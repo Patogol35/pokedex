af28318eb29057c93670fd1dcd0f14e1fab1cb3e
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-28 13:10:53 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,45 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import { getPokemons, getPokemonByName } from '../api/pokeapi'
-import PokemonCard from '../components/PokemonCard'
-import Pagination from '../components/Pagination'
-import Search from '../components/Search'
+import React from "react";
+import PokemonCard from "../components/PokemonCard";
+import Pagination from "../components/Pagination";
+import Search from "../components/Search";
+import usePokemons from "../hooks/usePokemons";
 
 export default function Home() {
-  const [pokemons, setPokemons] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [page, setPage] = useState(1)
-  const limit = 20
-  const totalPokemons = 151 // Primera generación
-  const totalPages = Math.ceil(totalPokemons / limit)
-
-  useEffect(() => {
-    fetchPokemons()
-  }, [page])
-
-  async function fetchPokemons() {
-    setLoading(true)
-    try {
-      const data = await getPokemons(limit, (page - 1) * limit)
-      const detailed = await Promise.all(
-        data.results.map(async (p) => await getPokemonByName(p.name))
-      )
-      setPokemons(detailed)
-    } catch (error) {
-      console.error(error)
-    }
-    setLoading(false)
-  }
-
-  async function handleSearch(name) {
-    setLoading(true)
-    try {
-      const result = await getPokemonByName(name)
-      setPokemons([result])
-    } catch (error) {
-      alert('Pokémon no encontrado')
-    }
-    setLoading(false)
-  }
+  const { pokemons, loading, page, setPage, totalPages, handleSearch } =
+    usePokemons(20, 151);
 
   return (
     <div>
@@ -47,12 +14,14 @@ export default function Home() {
       {loading ? (
         <p>Cargando...</p>
       ) : (
-        <div style={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(auto-fit, minmax(150px, 1fr))',
-          gap: '1rem'
-        }}>
-          {pokemons.map(p => (
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fit, minmax(150px, 1fr))",
+            gap: "1rem",
+          }}
+        >
+          {pokemons.map((p) => (
             <PokemonCard key={p.id} pokemon={p} />
           ))}
         </div>
@@ -63,5 +32,5 @@ export default function Home() {
         onPageChange={(p) => setPage(p)}
       />
     </div>
-  )
+  );
 }
